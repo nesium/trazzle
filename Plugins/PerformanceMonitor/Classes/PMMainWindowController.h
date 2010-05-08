2c14e8a04d408b19ac49f525d4ee23f8337e8247
mb <mb@nesium.com>
2010-05-08 20:55:15 +0200

    - removed unused code

--- Plugins/PerformanceMonitor/Classes/PMMainWindowController.h
+++ Plugins/PerformanceMonitor/Classes/PMMainWindowController.h
@@ -10,7 +10,6 @@
 #import <QuartzCore/QuartzCore.h>
 #import "PMStatsViewLayer.h"
 #import "PMStatsSessionViewLayer.h"
-#import "AGProcess.h"
 #import "PMMonitoringService.h"
 #import "ZZPlugInController.h"
 
